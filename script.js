56a96f06a7468affbd23c0d1093095fcd14fa999
Bao Xiong <bao.xiong2011@gmail.com>
2021-01-21 00:36:40 -0600

    Added rows back into html.

--- script.js
+++ script.js
@@ -4,11 +4,24 @@
 
 
 
-//use moment to get current date for header
+//use moment to get current date for header(works!)
 function dateHeader(){
     var currentDateHeader= moment().format("MMMM Do YYYY");
-    $("#currentDay").append(currentDateHeader);
+    $("#currentDay").text(currentDateHeader);
 }
 
 dateHeader();
+
+//display the current hour of the day (works!)
+
+function getCurrentTime(){
+    var currentTime = moment().format('H');
+    console.log(currentTime);
+}
+getCurrentTime();
+
+//make new row from 9am-6pm
+
+
+
 
